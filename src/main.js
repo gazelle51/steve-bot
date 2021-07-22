1c33ee062e54e0af141849a487e0a7c6c31e62ab
Sarah Gazelle <sarahgaz@au1.ibm.com>
2021-07-22 13:25:37 +1000

    add some more commands

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 
 const borat = require('./borat');
 const voice = require('./voice');
+const { prefix } = require('./config.json');
 const { Client } = require('discord.js');
 
 // Create Discord client
@@ -25,21 +26,78 @@ client.on('ready', () => {
  * Emitted whenever a message is created.
  */
 client.on('message', async (message) => {
-  // Do nothing if the message was from the bot
-  if (message.author === client.user) return;
+  // Do nothing if the message does not start with prefix or was from a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  // Ping message
-  if (message.content === '~ping') {
+  // Extract command and arguments
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  // Ping
+  if (message.content === `${prefix}ping`) {
     message.channel.send('pong');
   }
 
-  // Join voice
-  if (message.content === '~join' || message.content === 'hi steve') {
+  // Join voice channel
+  else if (message.content === `${prefix}join` || message.content === 'hi steve') {
     const connection = await voice.join(message);
 
     if (!connection) return;
 
     connection.play(borat.helloNiceToMeetYou);
+  }
+
+  // Get server details
+  else if (message.content === `${prefix}server`) {
+    if (!message.guild) return;
+    message.channel.send(
+      `Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\nCreated on: ${message.guild.createdAt}\nRegion: ${message.guild.region}`
+    );
+  }
+
+  // Get user info
+  else if (message.content === `${prefix}user-info`) {
+    message.channel.send(
+      `Your username: ${message.author.username}\nYour ID: ${message.author.id}`
+    );
+  }
+
+  // Extract arguments from a command
+  else if (command === 'args-info') {
+    if (!args.length)
+      return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+
+    message.channel.send(`Command name: ${command}\nArguments: ${args.join(', ')}`);
+  }
+
+  // Kick user
+  else if (command === 'kick') {
+    if (!message.mentions.users.size) {
+      return message.reply('you need to tag a user in order to kick them!');
+    }
+
+    // Grab the "first" mentioned user from the message
+    const taggedUser = message.mentions.users.first();
+
+    message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+  }
+
+  //TODO https://discordjs.guide/creating-your-bot/commands-with-user-input.html#working-with-multiple-mentions
+
+  // Display avatar of user
+  else if (command === 'avatar') {
+    if (!message.mentions.users.size) {
+      return message.channel.send(
+        `Your avatar: <${message.author.displayAvatarURL({ format: 'png', dynamic: true })}>`
+      );
+    }
+
+    // Grab the "first" mentioned user from the message
+    const taggedUser = message.mentions.users.first();
+
+    return message.channel.send(
+      `Avatar: <${taggedUser.displayAvatarURL({ format: 'png', dynamic: true })}>`
+    );
   }
 });
 

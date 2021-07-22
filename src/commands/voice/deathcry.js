e67368fb0a69f84ef1afec6e6cb8935de9bc7d79
gazelle51 <48930996+gazelle51@users.noreply.github.com>
2021-07-22 21:00:16 +1000

    add deathcry command

---
+++ src/commands/voice/deathcry.js
@@ -0,0 +1,26 @@
+const csgo = require('../../csgo');
+const voice = require('../../utils/voice');
+
+/**
+ * Execute deathcry command.
+ * @param {Object} message - Received message
+ * @param {*} args
+ */
+async function execute(message, args) {
+  // Join voice channel
+  const connection = await voice.join(message);
+
+  // Check connection was successful
+  if (!connection) return;
+
+  // Say hello
+  connection.play(csgo.deathcry);
+}
+
+module.exports = {
+  name: 'deathcry',
+  description: 'CSGO death cry',
+  guildOnly: true,
+  cooldown: 2,
+  execute,
+};
---
+++ src/csgo.js
@@ -0,0 +1,4 @@
+module.exports = {
+  deathcry:
+    'https://www.voicy.network/Content/Clips/Sound/aec38063-f72a-4c82-b45e-90294086637a.mp3',
+};

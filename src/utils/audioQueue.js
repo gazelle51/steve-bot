df27896214cf48fa7a6e2a036f21522bb6311590
gazelle51 <48930996+gazelle51@users.noreply.github.com>
2021-07-26 22:10:59 +1000

    add inactivity timeout

--- src/utils/audioQueue.js
+++ src/utils/audioQueue.js
@@ -21,6 +21,7 @@ function createServerQueue(client, message, voiceConnection, audio) {
     textChannel: message.channel,
     audioQueue: [audio],
     playing: true,
+    leaveInactive: null,
   };
   client.queue.set(message.guild.id, queueConstruct);
 }
@@ -39,9 +40,23 @@ function play(client, guild, audio) {
 
   // Check if there is audio to play
   if (!audio) {
-    serverQueue.voiceChannel.leave();
-    client.queue.delete(guild.id);
+    serverQueue.playing = false;
+
+    // Leave after 10 minutes of inactivity
+    serverQueue.leaveInactive = setTimeout(function () {
+      serverQueue.voiceChannel.leave();
+      client.queue.delete(guild.id);
+    }, 10 * 60 * 1000);
+
     return;
+  }
+
+  // Clear inactivity timer
+  serverQueue.playing = true;
+  try {
+    clearTimeout(serverQueue.leaveInactive);
+  } catch (e) {
+    console.log('Could not clear inactivity timeout');
   }
 
   // Dispatcher
@@ -56,6 +71,7 @@ function play(client, guild, audio) {
 
 /**
  * Add audio to queue and start playing.
+ * If queue is inactive, it will be resumed.
  * If a queue doesn't exist, one will be created.
  * @param {Object} client - Discord client
  * @param {Object} message - Received message
@@ -76,8 +92,13 @@ async function addAudio(client, message, audio) {
     // Create queue and start playing
     createServerQueue(client, message, connection, audio);
     play(client, message.guild, audio);
+  } else if (serverQueue && serverQueue.playing === false) {
+    // If there is a queue that is not playing, add to it and start again
+    serverQueue.audioQueue.push(audio);
+    console.log(`${audio.title} has been added to the queue`);
+    play(client, message.guild, audio);
   } else {
-    // If there is a queue, add to it
+    // If there is a queue that is playing, add to it
     serverQueue.audioQueue.push(audio);
     console.log(`${audio.title} has been added to the queue`);
   }

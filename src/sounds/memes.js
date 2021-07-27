8c7e0c47c116504d3beb3aed108df1c13074cded
gazelle51 <48930996+gazelle51@users.noreply.github.com>
2021-07-27 22:12:13 +1000

    Update memes.js

--- src/sounds/memes.js
+++ src/sounds/memes.js
@@ -3,4 +3,9 @@ module.exports = {
     'https://www.voicy.network/Content/Clips/Sound/b7ed76b9-4320-4e53-adc2-866d68f3643e.mp3',
   heyThatsPrettyGood:
     'https://www.voicy.network/Content/Clips/Sound/68a72694-dfed-494a-baa6-237093256e74.mp3',
+  sadTune: 'https://www.voicy.network/Content/Clips/Sound/736f9dfb-0d7c-4d91-b676-bceb0d101bca.mp3',
+  whyAreYouRunning:
+    'https://www.voicy.network/Content/Clips/Sound/1d25df69-d612-4d11-9b68-fad04f7249df.mp3',
+  deezNuts:
+    'https://www.voicy.network/Content/Clips/Sound/781f7a40-56cd-48d4-b474-0556793ae2c1.mp3',
 };

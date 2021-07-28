6e3580cb5f40083d0cd7a1489b96650322efd043
gazelle51 <48930996+gazelle51@users.noreply.github.com>
2021-07-28 19:59:31 +1000

    fix prefix

--- src/typedefs/discord.js
+++ src/typedefs/discord.js
@@ -3,6 +3,37 @@ const { Client, Collection } = require('discord.js');
 // TODO: update maps and collections
 
 /**
+ * Discord command
+ * @typedef {Object} Command
+ *   name: '',
+  description: '',
+  args: false,
+  usage: '',
+  guildOnly: false,
+  cooldown: 1,
+  aliases: [],
+  permissions: '',
+  execute,
+ */
+
+/**
+ * Discord command cooldown
+ * @typedef {Object} Cooldown
+ *   ?
+ */
+
+/**
+ * Discord audio queue for a guild
+ * @typedef {Object} AudioQueue
+ *       voiceChannel: message.member.voice.channel,
+    voiceConnection: voiceConnection,
+    textChannel: message.channel,
+    audioQueue: [audio],
+    playing: true,
+    leaveInactive: null,
+ */
+
+/**
  * Discord client
  * @typedef {Object} ClientExtension
  * @property {Collection} [commands] - Commands library

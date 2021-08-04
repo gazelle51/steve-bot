dc72c551f0360f028ef15a705e77f8e827544bc9
gazelle51 <48930996+gazelle51@users.noreply.github.com>
2021-08-04 21:33:19 +1000

    add basic embed

--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -1,4 +1,4 @@
-const { Message } = require('discord.js');
+const { Message, MessageEmbed } = require('discord.js');
 const queue = require('../../utils/audioQueue');
 
 /**
@@ -12,6 +12,24 @@ async function execute(message, args, client) {
   if (!message.member.voice.channel)
     return message.channel.send('You have to be in a voice channel to see the music!');
 
+  // https://v12.discordjs.guide/popular-topics/embeds.html#embed-preview
+  const exampleEmbed = new MessageEmbed()
+    .setColor('#23E5D6')
+    .setTitle('✨🎵 Music Queue 🎵✨')
+    .addFields(
+      {
+        name: 'Now playing',
+        value: 'Some value here',
+      },
+      {
+        name: 'Up next',
+        value: '1. Some value here\n2. Some value here\n3. Some value here\n\nx songs, y length',
+      }
+    )
+    .setTimestamp()
+    .setFooter('Page 1/1');
+  message.channel.send(exampleEmbed);
+
   // Get queue
   const audioQueue = queue.getQueue(client, message);
 
@@ -22,10 +40,10 @@ async function execute(message, args, client) {
   const nowPlaying = audioQueue.shift();
 
   // Format string to send
-  let displayString = `**Now playing:** \`${nowPlaying.title}\` (${nowPlaying.length}), added by ${nowPlaying.addedBy}\n\n**Up next:**\n`;
+  let displayString = `**Now playing:** \`${nowPlaying.title}\` (${nowPlaying.length}), added by \`${nowPlaying.addedBy}\`\n\n**Up next:**\n`;
   for (const [i, audio] of audioQueue.entries()) {
     displayString = displayString.concat(
-      `${i + 1}. \`${audio.title}\` (${audio.length}), added by ${audio.addedBy}\n`
+      `${i + 1}. \`${audio.title}\` (${audio.length}), added by \`${audio.addedBy}\`\n`
     );
   }
 

f39ad757d08c33b76f76d6b489c39b293b83e366
gazelle51 <48930996+gazelle51@users.noreply.github.com>
2021-08-08 12:28:23 +1000

    refactor into functions

--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -1,4 +1,4 @@
-const { Message, MessageEmbed } = require('discord.js');
+const { Message, MessageEmbed, MessageReaction, User, TextChannel } = require('discord.js');
 const queue = require('../../utils/audioQueue');
 
 /**
@@ -15,30 +15,41 @@ async function execute(message, args, client) {
   // Get queue
   const audioQueue = queue.getQueue(client, message);
 
-  // Create embed
-  const queueEmbed = createQueueEmbed();
-
   // Check if the queue is empty
   if (!audioQueue.length) {
-    queueEmbed.addField(
-      "There's nothing here",
-      '​No music is being played 😭! Add some music to the queue using the `play` command.​'
-    );
-    return message.channel.send(queueEmbed);
+    return message.channel.send(emptyQueueEmbed());
   }
 
   // Get audio now playing
   const nowPlaying = audioQueue.shift();
 
-  // Add now playing to the embed
-  queueEmbed.addField(
+  // Display results in text channel
+  message.channel.send(queueEmbed(nowPlaying, audioQueue));
+}
+
+function baseEmbed() {
+  return new MessageEmbed().setColor('#23E5D6').setTitle('✨🎵 Music Queue 🎵✨');
+}
+
+function emptyQueueEmbed() {
+  const embed = baseEmbed();
+  embed.addField(
+    "There's nothing here",
+    '​No music is being played 😭! Add some music to the queue using the `play` command.​'
+  );
+  return embed;
+}
+
+function queueEmbed(nowPlaying, upNext) {
+  const embed = baseEmbed();
+
+  embed.addField(
     'Now playing',
     `[${nowPlaying.title}](${nowPlaying.url}) (${nowPlaying.length}), added by \`${nowPlaying.addedBy}\`\n`
   );
 
-  // Add up next to the embed
-  const upNext = audioQueue.length
-    ? audioQueue
+  const upNextData = upNext.length
+    ? upNext
         .map(
           (audio, i) =>
             `${i + 1}. [${audio.title}](${audio.url}) (${audio.length}), added by \`${
@@ -47,19 +58,87 @@ async function execute(message, args, client) {
         )
         .join('\n')
     : `No more songs in the queue`;
-  queueEmbed.addField('Up next', `${upNext}\n\n**Songs in queue:** ${audioQueue.length}\n`);
+  embed.addField('Up next', `${upNextData}\n\n**Songs in queue:** ${upNext.length}\n`);
 
-  // Display results in text channel
-  message.channel.send(queueEmbed);
+  return embed;
 }
 
-function createQueueEmbed() {
-  // Set up embed
-  return new MessageEmbed()
-    .setColor('#23E5D6')
-    .setTitle('✨🎵 Music Queue 🎵✨')
-    .setTimestamp()
-    .setFooter('Page 1/1');
+/**
+ *
+ * @param {Object[]} pages - pages of data to display
+ * @param {string} authorId - ID of user who requested queue
+ * @param {TextChannel} channel - Discord channel queue is to be sent to
+ * @returns {Promise<void>}
+ */
+async function createAndSendEmbed(pages, authorId, channel) {
+  /**
+   * Create a queue message embed.
+   * @param {Object} pageData - page data to display
+   * @returns {MessageEmbed}
+   */
+  function createQueueEmbed(pageData) {
+    return new MessageEmbed()
+      .setColor('#23E5D6')
+      .setTitle('✨🎵 Music Embed 🎵✨')
+      .setDescription(pageData)
+      .setTimestamp()
+      .setFooter(`Page ${page} of ${pages.length}`);
+  }
+
+  /**
+   * Backward button reaction handler.
+   * Go back a page in the embed if not at the first page.
+   * @param {MessageReaction} reaction
+   * @param {User} user
+   * @returns {void}
+   */
+  const backwardHandler = (reaction, user) => {
+    if (page === 1) return;
+    page--;
+    embedMessage.edit(createQueueEmbed(pages[page - 1]));
+  };
+
+  /**
+   * Forward button reaction handler.
+   * Go forward a page in the embed if not at the last page.
+   * @param {MessageReaction} reaction
+   * @param {User} user
+   * @returns {void}
+   */
+  const forwardHandler = (reaction, user) => {
+    if (page === pages.length) return;
+    page++;
+    embedMessage.edit(createQueueEmbed(pages[page - 1]));
+  };
+
+  // Initialise
+  let page = 1;
+  const reactionTime = 120000;
+
+  // Create and send initial embed
+  const embed = createQueueEmbed(pages[0]);
+  const embedMessage = await channel.send(embed);
+  await Promise.all([embedMessage.react('⬅️'), embedMessage.react('➡️')]);
+
+  // Reaction filters
+  const backwardFilter = (reaction, user) => reaction.emoji.name === '⬅️' && user.id === authorId;
+  const forwardFilter = (reaction, user) => reaction.emoji.name === '➡️' && user.id === authorId;
+
+  // Reaction collectors
+  const backwards = embedMessage.createReactionCollector(backwardFilter, {
+    time: reactionTime,
+    dispose: true,
+  });
+  const forwards = embedMessage.createReactionCollector(forwardFilter, {
+    time: reactionTime,
+    dispose: true,
+  });
+
+  // Reaction events
+  backwards.on('collect', backwardHandler);
+  backwards.on('remove', backwardHandler);
+  forwards.on('collect', forwardHandler);
+  forwards.on('remove', forwardHandler);
 }
 
 module.exports = {

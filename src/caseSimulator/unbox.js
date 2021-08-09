f5172d31b6f9ad836848597b47234cc57772b61a
Sarah Gazelle <sarahgaz@au1.ibm.com>
2021-08-10 09:10:36 +1000

    comment all functions

--- src/caseSimulator/unbox.js
+++ src/caseSimulator/unbox.js
@@ -10,21 +10,29 @@ TODO
 - embed with requesting user
 */
 
+/**
+ * Unbox a CS-GO case.
+ * @param {string} caseName - name of case to unbox
+ * @returns {Object}
+ */
 function unbox(caseName) {
   const data = caseData.cases[0];
 
   console.log(`Unboxing: ${data.name}`);
 
   const colour = weaponColour();
-  const wear = weaponWear();
   const caseWeapon = weapon(data[colour]);
 
   caseWeapon.colour = colour;
-  caseWeapon.wear = wear;
+  caseWeapon.wear = weaponWear();
 
   return caseWeapon;
 }
 
+/**
+ * Generate a weapon colour based on the grade odds.
+ * @returns {string}
+ */
 function weaponColour() {
   const randomNumber = Math.random();
   const blueThreshold = caseData.gradeOdds.blue;
@@ -41,10 +49,19 @@ function weaponColour() {
   return colour;
 }
 
+/**
+ * Pick a random weapon from the proided weapon list.
+ * @param {Object[]} possibleWeapons
+ * @returns {Object}
+ */
 function weapon(possibleWeapons) {
   return _.sample(possibleWeapons);
 }
 
+/**
+ * Generate a weapon wear based on the wear odds.
+ * @returns {string}
+ */
 function weaponWear() {
   const randomNumber = Math.random();
   const wellWornThreshold = caseData.wearOdds.wellWorn;

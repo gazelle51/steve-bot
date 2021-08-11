55202b1baf2cb2f2647eae195abbb75d66b2fc33
Sarah Gazelle <sarahgaz@au1.ibm.com>
2021-08-11 19:54:04 +1000

    function to scrape case page

--- src/caseSimulator/webScraper.js
+++ src/caseSimulator/webScraper.js
@@ -1,20 +1,59 @@
 const axios = require('axios').default;
 const cheerio = require('cheerio');
 
-async function scrapeUrl(url) {
+async function scrapeCasePage(url) {
+  // Load page into Cheerio
   const html = (await axios.get(url)).data;
-
   const $ = cheerio.load(html);
 
   // Find all result boxes with a h3 inside of them
-  const data = $('div .result-box > h3');
+  $('div .result-box ')
+    .not((i, result) => !$(result).children('h3').length)
+    .each((i, result) => {
+      // Weapon name
+      const name = $(result).find('h3').text();
 
-  console.log(data);
-  console.log(data.length);
+      // Skip knives
+      if (name.includes('Knives')) return;
 
-  // https://zetcode.com/javascript/cheerio/
-  // https://stackoverflow.com/questions/58052001/get-html-source-code-from-a-website-and-then-get-an-element-from-the-html-file
-  // https://www.freecodecamp.org/news/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3/
+      // Rarity
+      const rarity = $(result)
+        .find('a[class=nounderline]')
+        .children('div .quality')
+        .attr('class')
+        .replace('quality color-', '');
+
+      // Colour (based on rarity)
+      const colourMap = {
+        covert: 'red',
+        classified: 'pink',
+        restricted: 'purple',
+        milspec: 'blue',
+      };
+      const colour = colourMap[rarity];
+
+      // Image URL
+      const imageUrl = $(result).find('a').children('img').attr('src');
+
+      // Price
+      const priceElements = $(result).find('div .price');
+      const price = $(priceElements)
+        .filter((i, elem) => $(elem).children().find('a[class="price-st"]').length === 0)
+        .text()
+        .trim();
+      const statTrakPrice = $(priceElements)
+        .filter((i, elem) => $(elem).children().find('a[class="price-st"]').length === 1)
+        .text()
+        .trim();
+
+      console.log(name);
+      console.log(rarity);
+      console.log(colour);
+      console.log(imageUrl);
+      console.log(price);
+      console.log(statTrakPrice);
+      console.log();
+    });
 }
 
-scrapeUrl('https://csgostash.com/case/4/CS:GO-Weapon-Case-2');
+scrapeCasePage('https://csgostash.com/case/4/CS:GO-Weapon-Case-2');

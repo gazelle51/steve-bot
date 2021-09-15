697ea8bc9abf107f4f1110a28849527b45cda6f6
Sarah Gazelle <sarahgazelle@hotmail.com>
2021-09-15 18:01:37 +1000

    script to get case data

--- src/scripts/getCaseData.js
+++ src/scripts/getCaseData.js
@@ -1,5 +1,27 @@
 /**
- * Run this script to get data on cases and weapons.
+ * Run this script to get data on weapons inside cases.
  *
- * To run use the `deploy-guild` script defined in the package.json.
+ * Weapons are collected for each case mentioned in ./caseSimulator/caseData.json
+ * and results are saved in ./scriptOutput/caseData.json
+ *
+ * To run use the `case-data` script defined in the package.json.
  */
+
+const caseData = require('../caseSimulator/caseData.json');
+const fs = require('fs');
+const scrapeCasePage = require('../caseSimulator/webScraper').scrapeCasePage;
+
+(async () => {
+  // Loop through cases
+  for (const caseObj of Object.values(caseData.cases)) {
+    // Get weapon data
+    const weapons = await scrapeCasePage(caseObj.url);
+    caseObj.weapons = weapons;
+  }
+
+  // Save updated data
+  if (!fs.existsSync('./scriptOutput')) {
+    fs.mkdirSync('./scriptOutput');
+  }
+  fs.writeFileSync('./scriptOutput/caseData.json', JSON.stringify(caseData, null, 2));
+})();

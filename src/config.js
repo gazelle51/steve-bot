84c6f7b25a08a624bd6fbba950c5276523919704
Sarah Gazelle <sarahgazelle@hotmail.com>
2022-07-13 17:57:06 +1000

    Add typedefs for disabled events and commands

--- src/config.js
+++ src/config.js
@@ -1,11 +1,17 @@
+/** @type {string[]} */
+const disabledCommands = [];
+
+/** @type {string[]} */
+const disabledEvents = [];
+
 /** @type {import("discord.js").HexColorString}} */
 const embedColour = '#23E5D6';
 
 module.exports = {
   prefix: '~',
   defaultCooldown: 0.1,
-  disabledCommands: [],
-  disabledEvents: [],
+  disabledCommands: disabledCommands,
+  disabledEvents: disabledEvents,
   embedColour: embedColour,
   emoji: {
     sparkles: '✨',
